7b46d960789a3690a3d18a2c0a506eea7a8e39c6
BernhardBaumrock <office@baumrock.com>
2022-01-19 15:27:54 +0100

    rename + bump version

--- RockMigrations1.module.php
+++ RockMigrations1.module.php
@@ -13,7 +13,7 @@ class RockMigrations1 extends WireData implements Module {
   public static function getModuleInfo() {
     return [
       'title' => 'RockMigrations1',
-      'version' => '0.0.87',
+      'version' => '0.0.88',
       'summary' => 'Module to handle Migrations inside your Modules easily.',
       'autoload' => 1, // the new migrations module has priority (2)
       'singular' => true,
